e72b941a5f2d18bcfa5282585087ee970ee64882
Mikołaj Mrózek <m.mik311@gmail.com>
2022-06-11 17:55:34 +0200

    Created index file for screeens

---
+++ src/screens/index.ts
@@ -0,0 +1,17 @@
+import DashboardScreen from "./Dashboard";
+import SportsScreen from "./Sports";
+import CompetitionsScreen from "./Competitions";
+import SchedulingScreen from "./Scheduling";
+import OrganisationsScreen from "./Organisations";
+import UsersScreen from "./Users";
+import Error404 from "./404";
+
+export {
+  DashboardScreen,
+  SportsScreen,
+  CompetitionsScreen,
+  SchedulingScreen,
+  OrganisationsScreen,
+  UsersScreen,
+  Error404,
+};

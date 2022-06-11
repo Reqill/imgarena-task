bfd4a8fe3c51daac8f42f3d70072728a6d1eb70b
Mikołaj Mrózek <m.mik311@gmail.com>
2022-06-11 20:39:06 +0200

    Applied styling to TopBar.tsx

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,7 +1,66 @@
-/*
-  icon for 'user avatar' can be found here: https://mui.com/material-ui/material-icons/
- */
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Brightness5RoundedIcon from "@mui/icons-material/Brightness5Rounded";
+import DarkModeRoundedIcon from "@mui/icons-material/DarkModeRounded";
+import {
+  Box,
+  PaletteMode,
+  Stack,
+  Switch,
+  useTheme,
+  SvgIcon,
+} from "@mui/material";
+import { ReactComponent as Logo } from "./logo.svg";
+import { FC } from "react";
 
-export const TopBar = () => {
-    return <div>Top bar component</div>
+interface Props {
+  toggleTheme: (mode: PaletteMode) => void;
+  theme: PaletteMode;
 }
+
+export const TopBar: FC<Props> = ({ toggleTheme, theme }) => {
+  // const bg = useTheme().appBar.main;
+  // Property 'appBar' does not exist on type 'Theme'.
+  const palette = useTheme().palette;
+  const bg = "#272727";
+
+  return (
+    <Box sx={{ background: bg, p: 0.75 }}>
+      <Stack direction='row' alignItems='center' justifyContent='space-between'>
+        <Stack direction='row'>
+          <Logo height='33px' />
+        </Stack>
+        <Stack alignItems='center' direction='row' spacing={2} sx={{ pr: 2 }}>
+          <Stack alignItems='center' direction='row'>
+            <Switch
+              checked={theme !== "light"}
+              onChange={() => toggleTheme(theme === "light" ? "dark" : "light")}
+              inputProps={{ "aria-label": "controlled" }}
+              size='small'
+              sx={{
+                color: palette.secondary.contrastText,
+                "& .MuiSwitch-track": {
+                  backgroundColor: palette.secondary.contrastText,
+                },
+              }}
+            />
+            {theme === "light" ? (
+              <Brightness5RoundedIcon
+                fontSize='small'
+                sx={{ color: palette.secondary.contrastText, opacity: 0.95 }}
+              />
+            ) : (
+              <DarkModeRoundedIcon
+                fontSize='small'
+                sx={{ color: palette.secondary.contrastText, opacity: 0.95 }}
+              />
+            )}
+          </Stack>
+          <AccountCircleIcon
+            fontSize='large'
+            sx={{ color: palette.secondary.contrastText, opacity: 0.85 }}
+          />
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};

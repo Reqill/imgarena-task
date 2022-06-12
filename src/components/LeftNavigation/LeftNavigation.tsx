4a361b7b3a8d01b612fa1238da562e4209f460c3
Mikołaj Mrózek <m.mik311@gmail.com>
2022-06-12 12:37:13 +0200

    added styling to LeftNavigation

--- src/components/LeftNavigation/LeftNavigation.tsx
+++ src/components/LeftNavigation/LeftNavigation.tsx
@@ -1,31 +1,59 @@
-import { Divider, Paper, Stack } from "@mui/material";
+import { Divider, Paper, Stack, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import { NAVIGATION_ROUTES, CATEGORIES } from "../../navigationRoutes";
+import { NavigationItem } from "./NavigationItem";
 /*
   icons can be found in here: https://mui.com/material-ui/material-icons/
  */
 export const LeftNavigation = () => {
+  const palette = useTheme().palette;
+
   const _generateLinks = (category: string) => {
     return Object.keys(NAVIGATION_ROUTES)
       .filter((key) => NAVIGATION_ROUTES[key].category === category)
       .map((page) => (
-        <Link to={NAVIGATION_ROUTES[page].path} key={page}>
-          <p style={{ textTransform: "capitalize" }}>{page.toLowerCase()}</p>
-        </Link>
+        <NavigationItem
+          key={page}
+          to={NAVIGATION_ROUTES[page].path}
+          icon={NAVIGATION_ROUTES[page].icon}
+          label={page.toLowerCase()}
+        />
       ));
   };
 
   return (
-    <Stack direction='column'>
-      {Object.values(CATEGORIES).map((category, idx) => (
-        <div key={category}>
-          <p style={{ textTransform: "capitalize" }}>
-            {category.toLowerCase()}
-          </p>
-          {_generateLinks(category)}
-          {idx < Object.keys(CATEGORIES).length - 1 ? <Divider /> : null}
-        </div>
-      ))}
-    </Stack>
+    <Paper
+      square
+      elevation={2}
+      sx={{
+        height: "100%",
+        pt: 3,
+        backgroundColor: palette.background.paper,
+      }}
+    >
+      <Stack direction='column'>
+        {Object.values(CATEGORIES).map((category, idx) => (
+          <div key={category}>
+            <Typography
+              variant='subtitle1'
+              fontSize='12px'
+              sx={{
+                textTransform: "capitalize",
+                color: palette.grey["600"],
+                pl: 3.1,
+                pb: 0.5,
+                pt: 1.75,
+              }}
+            >
+              {category.toLowerCase()}
+            </Typography>
+            {_generateLinks(category)}
+            {idx < Object.keys(CATEGORIES).length - 1 ? (
+              <Divider sx={{ mt: 2.5 }} />
+            ) : null}
+          </div>
+        ))}
+      </Stack>
+    </Paper>
   );
 };
--- src/components/LeftNavigation/NavigationItem.tsx
+++ src/components/LeftNavigation/NavigationItem.tsx
@@ -1,7 +1,13 @@
-import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import {
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import { SvgIconComponent } from "@mui/icons-material";
-import { Link } from "react-router-dom";
-import { FC, ReactElement } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { FC, ReactElement, useState, useEffect } from "react";
 
 type NavigationItemProp = {
   to: string;
@@ -10,11 +16,42 @@ type NavigationItemProp = {
 };
 
 export const NavigationItem: FC<NavigationItemProp> = ({ to, icon, label }) => {
+  const [isActive, setIsActive] = useState(false);
+  const palette = useTheme().palette;
+
+  let path = useLocation().pathname;
+
+  useEffect(() => {
+    setIsActive(path === to);
+  }, [path]);
+
   return (
-    <Link to={to}>
-      <ListItemButton>
-        <ListItemIcon>{icon}</ListItemIcon>
-        <ListItemText>{label}</ListItemText>
+    <Link to={to} style={{ textDecoration: "none" }}>
+      <ListItemButton
+        color='primary'
+        selected={isActive}
+        dense
+        sx={{ py: 0.8, color: palette.primary.main }}
+      >
+        <ListItemIcon
+          sx={{
+            color: isActive ? palette.primary.main : palette.grey["600"],
+            minWidth: "17%",
+            ml: 1,
+          }}
+        >
+          {icon}
+        </ListItemIcon>
+        <ListItemText sx={{ textTransform: "capitalize" }}>
+          <Typography
+            variant='subtitle2'
+            fontSize='12px'
+            color={isActive ? palette.text.primary : palette.grey["700"]}
+            sx={{ mb: -0.25 }}
+          >
+            {label}
+          </Typography>
+        </ListItemText>
       </ListItemButton>
     </Link>
   );

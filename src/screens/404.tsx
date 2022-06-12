dd1cc1abab3b2208325bffb44b90691f3d334fed
Mikołaj Mrózek <m.mik311@gmail.com>
2022-06-12 12:35:25 +0200

    Error 404 now shows invalid path + styling

--- src/screens/404.tsx
+++ src/screens/404.tsx
@@ -1,5 +1,34 @@
+import { useLocation } from "react-router-dom";
+
 const Error404 = () => {
-  return <div>Page not found 404</div>;
+  let path = useLocation().pathname;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90%",
+      }}
+    >
+      <h3 style={{ margin: "4px" }}>Error 404</h3>
+      <p style={{ margin: "4px" }}>
+        page{" "}
+        <code
+          style={{
+            backgroundColor: "#CCC",
+            padding: "3px",
+            borderRadius: "5px",
+          }}
+        >
+          {path}
+        </code>{" "}
+        doesn't exist
+      </p>
+    </div>
+  );
 };
 
 export default Error404;

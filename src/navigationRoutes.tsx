621cbd0acd4ba709da3b7ff770a01f40ba68edba
Mikołaj Mrózek <m.mik311@gmail.com>
2022-06-12 12:37:49 +0200

    Added icons to NavigationRoutes

--- src/navigationRoutes.tsx
+++ src/navigationRoutes.tsx
@@ -6,6 +6,14 @@ import {
   OrganisationsScreen,
   UsersScreen,
 } from "./screens/index";
+import { SvgIconComponent } from "@mui/icons-material";
+import { ReactElement } from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import FactCheckIcon from "@mui/icons-material/FactCheck";
+import LocationCityIcon from "@mui/icons-material/LocationCity";
+import SupervisedUserCircleOutlinedIcon from "@mui/icons-material/SupervisedUserCircleOutlined";
 
 export const CATEGORIES = {
   management: "management",
@@ -13,10 +21,13 @@ export const CATEGORIES = {
   people: "people",
 };
 
+const ICON_SIZE = "20px";
+
 type NavigationRoute = {
   path: string;
   element: JSX.Element;
   category: string;
+  icon: ReactElement<SvgIconComponent>;
 };
 type NavigationRoutes = Record<string, NavigationRoute>;
 
@@ -25,30 +36,36 @@ export const NAVIGATION_ROUTES: NavigationRoutes = {
     path: "/",
     element: <DashboardScreen />,
     category: CATEGORIES.management,
+    icon: <HomeIcon sx={{ fontSize: ICON_SIZE }} />,
   },
   sports: {
     path: "/sports",
     element: <SportsScreen />,
     category: CATEGORIES.management,
+    icon: <SportsSoccerIcon sx={{ fontSize: ICON_SIZE }} />,
   },
   competitions: {
     path: "/competitions",
     element: <CompetitionsScreen />,
     category: CATEGORIES.management,
+    icon: <EmojiEventsIcon sx={{ fontSize: ICON_SIZE }} />,
   },
   scheduling: {
     path: "/scheduling",
     element: <SchedulingScreen />,
     category: CATEGORIES.planning,
+    icon: <FactCheckIcon sx={{ fontSize: ICON_SIZE }} />,
   },
   organisations: {
     path: "/organisations",
     element: <OrganisationsScreen />,
     category: CATEGORIES.planning,
+    icon: <LocationCityIcon sx={{ fontSize: ICON_SIZE }} />,
   },
   users: {
     path: "/users",
     element: <UsersScreen />,
     category: CATEGORIES.people,
+    icon: <SupervisedUserCircleOutlinedIcon sx={{ fontSize: ICON_SIZE }} />,
   },
 };
